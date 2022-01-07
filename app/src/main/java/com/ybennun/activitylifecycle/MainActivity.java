c47bdc1bb5b4d8593bbafd986a460945126e73ba
YeshayahuBennun <ybennun@yahoo.com>
2022-01-07 14:21:59 +0200

    commit#2 - Activity Life Cycle - Showing Activities Life Cycle

--- app/src/main/java/com/ybennun/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/ybennun/activitylifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.ybennun.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("Cycle","onCreate:");
         Toast.makeText(MainActivity.this, "onCreate() Called", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Cycle", "onStart:");
+        Toast.makeText(MainActivity.this, "onStart() Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("Cycle", "onPostResume:");
+        Toast.makeText(MainActivity.this, "onPostResume() Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Cycle", "onPaused:");
+        Toast.makeText(MainActivity.this, "onPaused() Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Cycle", "onStop:");
+        Toast.makeText(MainActivity.this, "onStop() Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Cycle", "onDestroy:");
+        Toast.makeText(MainActivity.this, "onDestroy() Called", Toast.LENGTH_SHORT).show();
     }
 }
